5418dec68ca2c149eb7f79169b92dd5d3910bdc7
seheerst <seherst9@gmail.com>
2023-10-20 21:25:52 +0300

    feat: generate entity

---
+++ EasyCashProjectEntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashProjectEntityLayer.Concrete
+{
+    public class CustomerAccount
+    {
+        public int CustomerAccountID { get; set; }
+        public string CustomerAccountNumber { get; set; }
+        public string CustomerAccountCurrency { get; set; }
+        public decimal CustomerAccountBalance { get; set; }
+        public string CustomerAccountBankBranch { get; set; }
+
+    }
+}
