4b7ccddfce290bcaf11ee189dadb24912349e43b
seheerst <seherst9@gmail.com>
2023-10-24 22:21:42 +0300

    feat: created services

---
+++ EasyCashProjectBusinessLayer/Abstract/ICustomerAccountProcessService.cs
@@ -0,0 +1,13 @@
+﻿using EasyCashProjectEntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashProjectBusinessLayer.Abstract
+{
+    internal interface ICustomerAccountProcessService : IGenericService<CustomerAccountProcess>
+    {
+    }
+}
