b1f9ef7c37e92c3e5224af5b99d9b1c73c932608
seheerst <seherst9@gmail.com>
2023-10-24 22:22:24 +0300

    feat: generate service class

---
+++ EasyCashProjectBusinessLayer/Concrete/CustomerAccountProcessManager.cs
@@ -0,0 +1,46 @@
+﻿using EasyCashProjectBusinessLayer.Abstract;
+using EasyCashProjectDataAccessLayer.Abstract;
+using EasyCashProjectEntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashProjectBusinessLayer.Concrete
+{
+    internal class CustomerAccountProcessManager : ICustomerAccountProcessService
+    {
+        private readonly ICustomerAccountProcessDal _customerAccountProcessDal;
+
+        public CustomerAccountProcessManager(ICustomerAccountProcessDal customerAccountProcessDal)
+        {
+            _customerAccountProcessDal = customerAccountProcessDal;
+        }
+
+        public void TDelete(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Delete(t);
+        }
+
+        public CustomerAccountProcess TGetByID(int id)
+        {
+            return _customerAccountProcessDal.GetByID(id);
+        }
+
+        public List<CustomerAccountProcess> TGetList()
+        {
+           return _customerAccountProcessDal.GetList();
+        }
+
+        public void TInsert(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Insert(t);
+        }
+
+        public void TUpdate(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Update(t);
+        }
+    }
+}
