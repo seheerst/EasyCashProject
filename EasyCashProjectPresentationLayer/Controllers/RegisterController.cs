62ec51a0e9f31e1e41cf48df09bcb9346d2878c5
seheerst <seherst9@gmail.com>
2023-10-25 21:36:49 +0300

    feat: create controller for register

---
+++ EasyCashProjectPresentationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,58 @@
+﻿using EasyCashProjectDtoLayer.Dtos.AppUserDtos;
+using EasyCashProjectEntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashProjectPresentationLayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserRegisterDto user)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    UserName = user.UserName,
+                    Name = user.Name,
+                    Surname = user.Surname,
+                    Email = user.Email,
+                    City = "null",
+                    District= "null",
+                    ImageUrl = "null"
+                    
+                };
+                var result = await _userManager.CreateAsync(appUser, user.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "ConfirmMail");
+                }
+                else
+                {
+                    foreach(var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+
+            }
+            return View();
+        }
+    }
+}
