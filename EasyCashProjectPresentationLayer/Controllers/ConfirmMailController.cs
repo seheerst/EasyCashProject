0a8499c7dc3ab321a787fa21a8daecdefa824da8
seheerst <seherst9@gmail.com>
2023-10-27 21:20:46 +0300

    feat: create controller for confirm mail

---
+++ EasyCashProjectPresentationLayer/Controllers/ConfirmMailController.cs
@@ -0,0 +1,23 @@
+﻿
+using EasyCashProjectPresentationLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashProjectPresentationLayer.Controllers
+{
+	public class ConfirmMailController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index(int id)
+		{
+			var value = TempData["Mail"];
+			ViewBag.v = value + " aaa";
+			return View();
+		}
+
+        [HttpPost]
+        public IActionResult Index(ConfirmMailViewModel model)
+        {
+            return View();
+        }
+    }
+}
