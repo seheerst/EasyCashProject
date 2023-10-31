18fce2bbbcd0a13d2433b11d9e3bfcac8a63de97
seheerst <seherst9@gmail.com>
2023-10-31 20:47:19 +0300

    feat: create controller for profile page

---
+++ EasyCashProjectPresentationLayer/Controllers/MyProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashProjectPresentationLayer.Controllers
+{
+    public class MyProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
