618cc5a71807ff4cfccdb85204e587768b4d9aa9
seheerst <seherst9@gmail.com>
2023-10-31 20:46:58 +0300

    feat: mail confirm done

--- EasyCashProjectPresentationLayer/Controllers/ConfirmMailController.cs
+++ EasyCashProjectPresentationLayer/Controllers/ConfirmMailController.cs
@@ -1,23 +1,39 @@
 ﻿
+using EasyCashProjectEntityLayer.Concrete;
 using EasyCashProjectPresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCashProjectPresentationLayer.Controllers
 {
 	public class ConfirmMailController : Controller
 	{
-		[HttpGet]
-		public IActionResult Index(int id)
+		private readonly UserManager<AppUser> _userManager;
+
+        public ConfirmMailController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+		public IActionResult Index()
 		{
 			var value = TempData["Mail"];
-			ViewBag.v = value + " aaa";
+            ViewBag.v = value;
 			return View();
 		}
 
         [HttpPost]
-        public IActionResult Index(ConfirmMailViewModel model)
+        public async Task<IActionResult> Index(ConfirmMailViewModel model)
         {
-            return View();
+            var user = await _userManager.FindByEmailAsync(model.Mail);
+            if(user.ConfirmCode == model.ConfirmCode)
+            {
+                user.EmailConfirmed = true;
+                await _userManager.UpdateAsync(user);
+                return RedirectToAction("Index", "MyProfile");
+            }
+			return View();
         }
     }
 }
