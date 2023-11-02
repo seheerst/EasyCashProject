6c4fd066db7f5590354872524a042843d602945d
seheerst <seherst9@gmail.com>
2023-11-02 21:10:57 +0300

    feat: created view components for profile page

---
+++ EasyCashProjectPresentationLayer/ViewComponents/Customer/_CustomerLayoutContentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashProjectPresentationLayer.ViewComponents.Customer
+{
+    public class _CustomerLayoutContentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
